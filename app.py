52c191969cc3168d592834173c2bfcfa80d23193
wentingxiong <wtx0321@gmail.com>
2018-08-06 14:06:22 +0200

    change import

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ import pandas as pd
 import numpy as np
 import os
 from random import randint
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-from utils.heatmap_minmax import getRGB, rescale_score_by_abs
+# from utils.heatmap_minmax import getRGB, rescale_score_by_abs
 
 import pickle
 
 
+matplotlib.use("agg")
 # app = dash.Dash("attr")
 # server = app.server
 
@@ -45,6 +46,46 @@ app.scripts.config.serve_locally = False
 
 cmap_name="bwr"
 colormap  = plt.get_cmap(cmap_name)
+
+
+def rescale_score_by_abs(score, max_score, min_score):
+    """
+    rescale positive score to the range [0.5, 1.0], negative score to the range [0.0, 0.5],
+    using the extremal scores max_score and min_score for normalization
+    """
+
+    # CASE 1: positive AND negative scores occur --------------------
+    if max_score > 0 and min_score < 0:
+
+        if max_score >= abs(min_score):  # deepest color is positive
+            if score >= 0:
+                return 0.5 + 0.5 * (score / max_score)
+            else:
+                return 0.5 - 0.5 * (abs(score) / max_score)
+
+        else:  # deepest color is negative
+            if score >= 0:
+                return 0.5 + 0.5 * (score / abs(min_score))
+            else:
+                return 0.5 - 0.5 * (score / min_score)
+
+                # CASE 2: ONLY positive scores occur -----------------------------
+    elif max_score > 0 and min_score >= 0:
+        if max_score == min_score:
+            return 1.0
+        else:
+            return 0.5 + 0.5 * (score / max_score)
+
+    # CASE 3: ONLY negative scores occur -----------------------------
+    elif max_score <= 0 and min_score < 0:
+        if max_score == min_score:
+            return 0.0
+        else:
+            return 0.5 - 0.5 * (score / min_score)
+
+def getRGB (c_tuple):
+    return "#%02x%02x%02x"%(int(c_tuple[0]*255), int(c_tuple[1]*255), int(c_tuple[2]*255))
+
 
 def ConditionalTable(df, max_value, min_value):
     # max_value = df.max(numeric_only=True).max()
